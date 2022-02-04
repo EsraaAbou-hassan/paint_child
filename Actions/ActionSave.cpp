7b00024fa9a104334bde0c9aa72128b7df194b91
mohamed najeeb <mohamednajeeb300@gmail.com>
2022-02-04 20:09:49 +0200

    Save Behaviour modified

--- Actions/ActionSave.cpp
+++ Actions/ActionSave.cpp
@@ -4,7 +4,8 @@
 #include "..\ApplicationManager.h"
 #include "..\GUI\GUI.h"
 #include <iostream>
-#include <string>
+#include <windows.h>
+#include <string.h>
 #include <fstream>
 
 
@@ -14,48 +15,94 @@ ActionSave::ActionSave(ApplicationManager* pApp,bool withLoading,bool forPlay) :
 	 playing = forPlay;
 }
 
+// Open Save File
+void ActionSave::SaveFileName()
+{
+	
+	char* Name = NULL;
+	OPENFILENAME ofn;
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	Name = new char[MAX_PATH];
+	Name[0] = 0;
+	ofn.lpstrFile = Name;
+	ofn.hwndOwner = NULL;
+	ofn.nMaxFile = MAX_PATH;
+	ofn.lpstrFilter = "Text\0*.TXT\0";
+	ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+	ofn.lpstrDefExt = "conf";
+	if (GetSaveFileName(&ofn)) {
+		Filename = Name;
+	}
+	else {
+		printf("No Name");
+	};
+	delete[] Name;
+	Name = NULL;
+
+
+}
+
+
 
 //Execute the action
 void ActionSave::Execute()
 {
 
-	
+
 	GUI* g = pManager->GetGUI();
 	GfxInfo gf;
 	CFigure* f;
 	ofstream MyFile;
+	
+	
+	// - - - - - Playing Mode Behaviour - - - - -
 	if (playing) {
 		MyFile.open("playing.txt", ios::trunc);
+
 	}
+
+
+	/// - - - - - Normal Save Behaviour - - - - - 
+
 	else {
-		g->PrintMessage("Enter your text File Name");
-		string Filename = g->GetSrting();
+
+
+		g->PrintMessage("Create Save File");
+		 SaveFileName();
+		g->PrintMessage("Save succesfull to " + Filename);
+
 		if (Filename == "")
 		{
 			g->PrintMessage("Wrong file name");
 			return;
 		}
-		MyFile.open(Filename + ".txt", ios::app);
+
+
+		MyFile.open(Filename, ios::out | ios::trunc);
+		string FillColor;
+		if (UI.isFilled == false) {
+			FillColor = "NO_FILL";
+		}
+		else {
+			FillColor = g->ConvertColorToString(UI.FillColor);
+		}
+		MyFile << g->ConvertColorToString(UI.DrawColor) << "\t" << FillColor << "\t" << g->ConvertColorToString(UI.BkGrndColor) << endl;
+
+		pManager->SaveAll(MyFile);
+		MyFile.close();
+		g->ClearStatusBar();
+
+		
+		
 	}
-	string FillColor;
-	if (UI.isFilled == false) {
-		FillColor = "NO_FILL";
+	
+
+
+		if (loading) {
+			g->PrintMessage("load");
+			Action* pAct = new ActionLoad(pManager, false);
+			pAct->Execute();
+
+		}
 	}
-	else {
-		FillColor = g->ConvertColorToString(UI.FillColor);
-	}
-	MyFile << g->ConvertColorToString(UI.DrawColor) << "\t" << FillColor << "\t" << g->ConvertColorToString(UI.BkGrndColor)<< endl;
-
-
-	pManager->SaveAll(MyFile);
-
-	MyFile.close();
-	g->ClearStatusBar();
-
-	if (loading) {
-		g->PrintMessage("load");
-		Action* pAct = new ActionLoad(pManager,false);
-		pAct->Execute();
-
-	}
-}
--- Actions/ActionSave.h
+++ Actions/ActionSave.h
@@ -7,12 +7,14 @@
 //SAVE SQUARE Action class
 class ActionSave : public Action
 {
+	string Filename;
 public:
 	bool loading;
 	bool playing;
 	ActionSave(ApplicationManager* pApp,bool withLoading, bool forPlay);
 
 	//Save  to the ApplicationManager
+	virtual void SaveFileName();
 	virtual void Execute();
 
 
